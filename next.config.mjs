5580e49aff82d29d5afa9f0220e337604487b901
Alan Delgado <7a.alan@gmail.com>
2024-06-14 21:31:03 -0600

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+import nextPWA from "next-pwa";
+
+const withPWA = nextPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
